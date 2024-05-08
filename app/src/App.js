0e43bccd4a36ec2645f1778ea44834df2c0f082d
Konstantin1spb <105925642+sitepreview@users.noreply.github.com>
2024-05-09 01:15:07 +0300

    practice-blog start

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+const App = () => {
+	return (
+		<>
+			<i className="fa fa-camera-retro fa-lg"></i>
+			<div>123</div>
+			<Div>123</Div>
+		</>
+	);
+};
+
+export default App;
